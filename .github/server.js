94ce5e520b1527fdbb10918e19567dcb52e03e01
KonstantinTechstarter <konstantin.kirsch@docc.techstarter.de>
2024-02-29 11:47:44 +0100

    update main.yml

---
+++ .github/server.js
@@ -0,0 +1,21 @@
+// Importiert das Express Framework in 
+// die Konstante Express
+const express = require("express");
+
+// Portnummer fuer den Server wird hier festgelegt!
+const PORT = process.env.PORT || 5500;
+
+// Express Anwendung wird erstellt und in
+// die Variable "app" importiert
+let app = express();
+
+// Alle Dateien die im Ordner "frontend" enthalten sind
+// werden an den User 'ohne weiter Verarbeitung' weitergeleitet.
+app.use(express.static('.github'));
+
+
+// Sobald Server gestartet ist, wird eine Meldeun
+// Konsole ausgeben
+app.listen(PORT, ()=> {
+    console.log("Server auf Port $(PORT)");
+});
